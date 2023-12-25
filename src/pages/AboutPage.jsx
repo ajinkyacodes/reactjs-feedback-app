f9d4f6f9bc30496d154665b99419e4fbf4d83cd1
AJINKYA <ajinkyacodes@gmail.com>
2023-12-26 01:12:29 +0530

    Created routing for home and about with about page basic page component

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About This Project</h1>
+      <br />
+      <p>This is a React app to leave feedback for a product or service</p>
+      <p>Version 1.0.0</p>
+      <p>&copy;ajinkyacodes</p>
+      <br />
+      <br />
+      <p>
+        <a href="/">Back To Home</a>
+      </p>
+    </Card>
+  );
+}
+
+export default AboutPage;
