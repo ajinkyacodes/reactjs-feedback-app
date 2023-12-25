b8c2cc8b46d9fff5448b735b54921945eb157fc7
AJINKYA <ajinkyacodes@gmail.com>
2023-12-26 03:18:43 +0530

    App imports sequence changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { FeedbackProvider } from "./context/FeedbackContext";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
-import AboutPage from "./pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
+import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
   return (
