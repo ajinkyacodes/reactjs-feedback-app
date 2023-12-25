e9ca688e31bf49c2cf84b6be32c3d108c6478ae4
AJINKYA <ajinkyacodes@gmail.com>
2023-12-25 21:18:47 +0530

    created separate data file for static feedback data

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolore commodi perferendis ea voluptas officiis",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolore commodi perferendis ea voluptas officiis",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi dolore commodi perferendis ea voluptas officiis",
+  },
+];
+
+export default FeedbackData;
