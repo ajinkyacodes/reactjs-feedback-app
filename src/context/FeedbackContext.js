115473c85be220423970e2398dad3b03a5673b33
AJINKYA <ajinkyacodes@gmail.com>
2023-12-26 18:31:24 +0530

    Created Update and Delete functionality for data coming from db.json file

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,12 +7,12 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
-    edit: false
-  })
+    edit: false,
+  });
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchFeedback();
-  },[]);
+  }, []);
 
   // Fetch feedback
   const fetchFeedback = async () => {
@@ -24,10 +24,10 @@ export const FeedbackProvider = ({ children }) => {
 
   // Add feedback
   const addFeedback = async (newFeedback) => {
-    const response = await fetch('/feedback', {
-      method: 'POST',
+    const response = await fetch("/feedback", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(newFeedback),
     });
@@ -38,23 +38,37 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
+
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Update feedback item
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item)=> item.id === id ? { ...item, ...updItem } : item));
-  }
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem)
+    });
+
+    const data = await response.json();
+
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
+    );
+  };
 
   // Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
       item,
-      edit:true
-    })
+      edit: true,
+    });
   };
 
   return (
